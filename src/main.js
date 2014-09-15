c0eb17788b1ba99f86ae19f9cf97911edb3ba08c
matt-herbstritt <matthew.herbstritt@gmail.com>
2014-09-15 20:58:57 +0700

    Initial commit.

---
+++ src/main.js
@@ -0,0 +1,14 @@
+/*global define */
+
+define(function (require) {
+    'use strict';
+
+    var devanagariTextField = require('textField');
+
+    //Return the module value.
+    return {
+        version: '0.0.1',
+        devanagariTextField: devanagariTextField
+    };
+    
+});
