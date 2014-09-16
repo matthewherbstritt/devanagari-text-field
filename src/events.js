d1aa9e7c9d49280d2325336399f46416d89c0d3b
matt-herbstritt <matthew.herbstritt@gmail.com>
2014-09-16 10:10:27 +0700

    Added helper functions taken from textField.js.

--- src/events.js
+++ src/events.js
@@ -1,9 +1,102 @@
-define([], function(){
+define(['util'], function(util){
+
+  var events = {};
 
 
-  var isValidCtrlCombo = function(ctrlKey, keyCode){
+  function keyHeldDown(tf){
+    return(tf.keyupCount !== tf.keydownCount)
+  }
+
+  function undoKeyHeldDown(tf){
+
+    var caretIndex    = util.getCaretIndex(tf.element),
+        newCaretIndex = caretIndex - tf.keydownCount;
+
+    tf.element.value  = tf.inputString;
+    tf.keydownCount   = 0;
+    tf.keyupCount     = 0;
+
+    setCaretIndex(tf, newCaretIndex);
+
+  }
+
+  function canIgnoreKeyCode(keyCode){
+     return([
+         0, // Firefox arrow keys
+         8, // Backspace
+         9, // Tab
+         13, // Enter
+         16, // Shift
+         17, // Control
+         18, // Alt
+         20, // Caps Lock
+         27, // Escape
+         33, // Page Up
+         34, // Page Down
+         35, // End
+         36, // Home
+         37, // Left Arrow
+         38, // Up Arrow
+         39, // Right Arrow
+         40, // Down Arrow
+         45, // Insert
+         46, // Delete
+         112, // F1
+         113, // F2
+         114, // F3
+         115, // F4
+         116, // F5
+         117, // F6
+         118, // F7
+         119, // F8
+         120, // F9
+         121, // F10
+         122, // F11
+         123, // F12
+         144, // Num Lock
+         255
+     ].indexOf(keyCode) > -1);
+  }
+
+
+  function toggleScript(tf){
+
+    var caretIndex  = util.getCaretIndex(tf.element);
+
+    if(tf.settings.scriptMode === 'Devanagari'){
+
+      tf.settings.scriptMode = 'Roman';
+      tf.key = null;
+      tf.cachedKey = null;
+      tf.cachedInputString = null;
+      tf.cachedInputStringKeyRemoved = null;
+      tf.keydownCount   = 0;
+      tf.keyupCount     = 0;
+
+    } else {
+
+      tf.settings.scriptMode = 'Devanagari';
+
+      tf.keydownCount = 0;
+      tf.keyupCount = 0;
+
+      tf.devanagariCharObj = null;
+
+      tf.key = null;
+      tf.cachedKey = null;
+
+      tf.inputString = tf.element.value;
+      tf.cachedInputString = null;
+
+      tf.inputStringKeyRemoved = null;
+      tf.cachedInputStringKeyRemoved = null;
+    }
+
+  }
+
+  function isValidCtrlCombo(ctrlKey, keyCode){
     return (
-      //(typeof ctrlKey !== undefined && typeof keyCode !== undefined) ||
+      (typeof ctrlKey !== undefined && typeof keyCode !== undefined) ||
       (ctrlKey && keyCode === 17) || // control keydown
       (ctrlKey && keyCode === 65) || // control + a = select all
       (ctrlKey && keyCode === 67) || // control + c = copy
@@ -14,9 +107,7 @@ define([], function(){
     );
   }
 
-
-
-  var incrementKeyEventCount = function(tf, eventType, keyCode, ctrlKey, shiftKey){
+  function incrementKeyEventCount(tf, eventType, keyCode, ctrlKey, shiftKey){
 
     var eventType = event.type,
         keyCode   = event.which || event.keyCode;
@@ -55,10 +146,16 @@ define([], function(){
         (tf.cachedKeyCode === 17 && tf.keyCode === 90)  // control keyup then z keyup
 
       ){ return; }
-    };
+    }
 
-
-
+    if(!canIgnoreKeyCode(keyCode)){
+      if(eventType === 'keyup'){
+        tf.keyupCount = tf.keyupCount += 1;
+      } else
+      if(eventType === 'keydown'){
+        tf.keydownCount = tf.keydownCount += 1;
+      }
+    }
 
   };
 
@@ -79,7 +176,7 @@ define([], function(){
     this.cachedKeyCode = this.keyCode;
     this.keyCode = keyCode;
 
-    incrementKeyEventCount(tf, eventType, keyCode, ctrlKey, shiftKey);
+    incrementKeyEventCount(this, eventType, keyCode, ctrlKey, shiftKey);
   }
 
   events.onKeyup = function(event){
